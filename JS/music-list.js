6ab9001a6512324f4e716e29c87d9f3b8bdd6e79
Max <maxbee.carrasco@outlook.com>
2022-11-08 09:03:38 -0300

    Reproductmp3

---
+++ JS/music-list.js
@@ -0,0 +1,44 @@
+
+let allMusic = [
+    {
+      name: "Somos los mismos de siempre",
+      artist: "La Renga",
+      src: "01 Somos los mismos de siempre - La Renga (Esquivando Charcos)"
+    },
+    {
+      name: "Moscas verdes para el charlatán",
+      artist: "La Renga",
+      src: "02 Moscas verdes para el charlatán - La Renga (Esquivando charcos)"
+    },
+    {
+      name: "Embrolos, fatos y paquetes",
+      artist: "La Renga",
+      src: "03 Embrolos, fatos y paquetes - La Renga (Esquivando charcos)"
+    },
+    {
+      name: "Luciendo mi saquito blusero",
+      artist: "La Renga",
+      src: "04 Luciendo mi saquito blusero - La Renga (Esquivando charcos)"
+    },
+    {
+      name: "Voy a bailar a la nave del olvido",
+      artist: "La Renga",
+      src: "05 Voy a bailar a la nave del olvido - La Renga (Esquivando charcos)"
+    },
+    {
+      name: "Buseca y vino tinto",
+      artist: "La Renga",
+      src: "06 Buseca y vino tinto - La Renga (Esquivando charcos)"
+    },
+    {
+        name: "El juicio del ganso - La Renga",
+        artist: "La Renga",
+        src: "07 El juicio del ganso - La Renga (Esquivando charcos)"
+    },
+    {
+        name: "Negra mi alma, negro mi corazón",
+        artist: "La Renga",
+        src: "08 Negra mi alma, negro mi corazón - La Renga (Esquivando charcos)"
+    },
+      
+  ];
